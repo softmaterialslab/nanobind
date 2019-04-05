e890fb2ca827a56fa6c2dab16e3b92c6d3b4ec4c
Lauren-Nilsson <lm44@iu.edu>
2019-04-05 16:39:29 -0400

    Added bond generation to preprocessor

--- preprocessing/main.cpp
+++ preprocessing/main.cpp
@@ -36,7 +36,8 @@ int main() {
   
   // Get important parameters from the user
   
-  double Kd, wall_dist, nano_conc;
+  double epsilon, wall_dist, nano_conc;
+  int num_particles;
   
   cout << "What is the [ligand - Virus] complex concentration (nM)?" << endl;
   cin >> nano_conc;
@@ -44,12 +45,17 @@ int main() {
   cout << "What is the receptor spacing (nm)?" << endl;
   cin >> wall_dist;
   
-  cout << "What is the binding strength of the ligand-receptor interaction (Kd)?" << endl;
-  cin >> Kd;
+  cout << "What is the binding strength of the ligand-receptor interaction (KbT)?" << endl;
+  cin >> epsilon;
+  
+  cout << "How many particles?" << endl;
+  cin >> num_particles;
   
 						//add options for ligand - virus ratio, properties of ligand / vlp, free ligand in solution
   
   // GENERATE P22CD40L.in FILE!!!
+  
+  // generate atoms
   
   ofstream datafile("P22CD40L.in");
   
@@ -62,16 +68,19 @@ int main() {
   double wall_diameter = 0.1; //reduced units
   
   double box_x, num_wall_x, num_atoms, num_bonds;
-  box_x = pow ( (1.793E-25)/(nano_conc * 1E-9) , (1.0/3.0) ); //in meters
+  box_x = pow ( (num_particles * 0.001 / (6.022E23))/(nano_conc * 1E-9) , (1.0/3.0) ); //in meters
   
    wall_dist = wall_dist * 1E-9; // in meters
   num_wall_x = box_x / wall_dist; //calculate number of mesh points
   num_wall_x = trunc(num_wall_x); // truncate this value
   cout << "adjusted spacing is " << (box_x/num_wall_x)*1E9 << " nm" << endl; //this is the adjusted spacing
-  num_atoms = (108 * 61) + (num_wall_x * num_wall_x);
-  num_bonds = 108 * 180;
+  num_atoms = (num_particles * 61) + (num_wall_x * num_wall_x);
+  num_bonds = num_particles * 180;
   
   double sigma = 60e-9; //sigma value currently used, diameter of P22
+  double sigma_hc = 0.12; // sigma hc in reduced units
+  double mass_SI = 3.819E-14; // mass of P22 in kg
+  double epsilon_SI = (1.38E-23) * (298.15); // epsilon in J
   box_x = box_x / sigma; // now in reduced units
   wall_dist = box_x / num_wall_x; // now in reduced units
   
@@ -88,11 +97,11 @@ int main() {
   
   datafile << "3 bond types" << endl;
   
-  datafile << -box_x/2 << "  " << box_x/2 << "  xlo  xhi" << endl; // in reduced units...
+  datafile << "0" << " " << box_x << " xlo xhi" << endl; // in reduced units...
   
-  datafile << -box_x/2 << "  " << box_x/2 << "  ylo  yhi" << endl;
+  datafile << "0" << " " << box_x << " ylo yhi" << endl;
   
-  datafile << -box_x/2 << "  " << box_x/2 << "  zlo  zhi" << endl;
+  datafile << "0" << " " << box_x << " zlo zhi" << endl;
   
   datafile << endl;
   
@@ -107,13 +116,13 @@ int main() {
     
    ////////////////////////////...To start on a lattice, use these lines.../////////////////////////////////////////
   
-  unsigned int num_fill = int(ceil(pow((double(108)), 1.0 / 3.0)));
+  unsigned int num_fill = int(ceil(pow((double(num_particles)), 1.0 / 3.0)));
   
 //     for (unsigned int i = 0; i < num_fill; i++) {
 //       for (unsigned int j = 0; j < num_fill; j++) {
 // 	for (unsigned int k = 0; k < num_fill; k++) {
 // 	  index += 1;
-// 	  if ( (i+j+k) < 108 ) {
+// 	  if ( (i+j+k) < num_particles ) {
 // 	    x_vlp = (((double)i*box_x*(1/(double)num_fill)));
 // 	    y_vlp = (((double)j*box_x*(1/(double)num_fill)));
 // 	    z_vlp = (((double)k*(box_x - (vlp_diameter*2.5/2))*(1/(double)num_fill))) + (vlp_diameter*2.5);
@@ -140,7 +149,7 @@ int main() {
   VECTOR3D dist;
   bool add;
   
-  while (vlp_xyz.size() < 108) {
+  while (vlp_xyz.size() < num_particles) {
     add = true;
     x_vlp = (gsl_rng_uniform(r)) * box_x; // guess some coordinates
     y_vlp = (gsl_rng_uniform(r)) * box_x;
@@ -153,13 +162,13 @@ int main() {
     if (vlp_xyz.size() == 0) {		//if no vlps have been added yet, add it
       vlp_xyz.push_back(VECTOR3D(x_vlp, y_vlp, x_vlp));
       index += 1;
-      datafile << index << "  " << "1" << "  " << x_vlp << "  " << y_vlp << "  " << z_vlp << "  " << endl;
+      datafile << index << " 1 " << "1" << " " << x_vlp << " " << y_vlp << " " << z_vlp << " " << endl;
       for (unsigned int m = 0; m < ligand_xyz.size(); m++){
 	index += 1;
 	x = x_vlp + ligand_xyz[m].x;
 	y = y_vlp + ligand_xyz[m].y;
 	z = z_vlp + ligand_xyz[m].z;
-	datafile << index << "  " << "2" << "  " << x << "  " << y << "  " << z << "  " << endl;
+	datafile << index << " 1 " << "2" << " " << x << " " << y << " " << z << " " << endl;
       }
     }
     
@@ -187,13 +196,13 @@ int main() {
   if (add == true){		//if it doesn't intersect, add it and its ligands to the list
     vlp_xyz.push_back(VECTOR3D(x_vlp, y_vlp, z_vlp));
     index += 1;
-    datafile << index << "  " << "1" << "  " << x_vlp << "  " << y_vlp << "  " << z_vlp << "  " << endl;
+    datafile << index << "  1 " << "1" << "  " << x_vlp << "  " << y_vlp << "  " << z_vlp << "  " << endl;
     for (unsigned int m = 0; m < ligand_xyz.size(); m++){
       index += 1;
       x = x_vlp + ligand_xyz[m].x;
       y = y_vlp + ligand_xyz[m].y;
       z = z_vlp + ligand_xyz[m].z;
-      datafile << index << "  " << "2" << "  " << x << "  " << y << "  " << z << "  " << endl;
+      datafile << index << "  1 " << "2" << "  " << x << "  " << y << "  " << z << "  " << endl;
     }
   } //else cout << "rejected!" << endl;
   
@@ -206,14 +215,52 @@ int main() {
     for (unsigned int i = 0; i < num_wall_x; i++){
       for (unsigned int j = 0; j < num_wall_x; j++){
 	index += 1;
-	datafile << index << "  " << "3" << "  " << i * wall_dist << "  " << j * wall_dist << "  " << "0" << endl;
+	datafile << index << " 1 " << "3" << "  " << i * wall_dist << "  " << j * wall_dist << "  " << "0" << endl;
       }
     }
     
-    cout << "Done!" << endl;
+    cout << "Done making atoms!" << endl;
+    
+    // Generate bonds
       
-      
+    vector<VECTOR3D> ligand_bond;
+    int type, b1, b2;
+    
+      ifstream bonds;                                      //open coordinates file
+    bonds.open("ligand_bond.coords");
+    if (!bonds) {                                        //check to make sure file is there
+        cerr << "ERR: FILE ligand_bond.coords NOT OPENED. Check directory and/or filename.";
+        exit(1);
+    }
+    for (int i = 0; i < 180; i++){
+      bonds >> index >> type >> b1 >> b2;				// Add coordinates the the vector array
+      ligand_bond.push_back(VECTOR3D(type,b1,b2));
+    }
 
+    datafile << endl << "Bonds" << endl << endl;
+    
+     index = 0;
+    
+    for (int i = 0; i < num_particles; i++){
+      for (int j = 0; j < ligand_bond.size(); j++){
+	index += 1;	
+	datafile << index <<  "     " << ligand_bond[j].x <<  "     " << ligand_bond[j].y + (i * 61) << "     " << ligand_bond[j].z + (i * 61) << endl;	
+      }
+      
+    }
+    
+    //bond type 1 length
+    dist = VECTOR3D(0,0,0) - ligand_xyz[0];
+    double type1_length = dist.GetMagnitude();
+    //bond type 2 length
+    dist = ligand_xyz[0] - ligand_xyz[1];
+    double type2_length = dist.GetMagnitude();
+    //bond type 3 length
+    dist = ligand_xyz[0] - ligand_xyz[20];
+    double type3_length = dist.GetMagnitude();
+    
+    
+    cout << "Done making bonds!" << endl;
 
   
   
@@ -234,7 +281,7 @@ int main() {
   
   outfile << "#Lammps input script for nanoparticle binding with multivalency #" << endl << endl;
   
-  outfile << "units lj" << endl << "boundary f p p" << endl << "atom_style bond " << endl << "neighbor 0.3 bin" << endl << "neigh_modify every 1 delay 0 check yes" << endl << endl;
+  outfile << "units lj" << endl << "boundary p p f" << endl << "atom_style bond " << endl << "neighbor 0.3 bin" << endl << "neigh_modify every 1 delay 0 check yes" << endl << endl;
   
   //Creating simulation box
   
@@ -260,7 +307,7 @@ int main() {
   
   outfile << "## Fixing Wall particles ##" << endl;
   
-  outfile << "fix xwalls all wall/reflect xhi EDGE xlo EDGE" << endl << endl;
+  outfile << "fix zwalls all wall/reflect zhi EDGE zlo EDGE" << endl << endl;
   
   //Interparticle potentials
   
@@ -268,17 +315,17 @@ int main() {
   
   outfile << "pair_style      hybrid lj/expand        2.5     lj/cut 2.5" << endl;
   
-  outfile << "pair_coeff      1       1       lj/cut           1      1.62     1.8183       # epsilon, sigma, cut-off  V-V" << endl;
+  outfile << "pair_coeff      1       1       lj/cut           1      " << vlp_diameter << "	" << vlp_diameter*1.12246   << "       # epsilon, sigma, cut-off  V-V" << endl;
   
-  outfile << "pair_coeff      1       2       lj/expand        1      0.12     0.74   0.97  # epsilon, sigma_hc, delta_V-L, cut-off" << endl;
+  outfile << "pair_coeff      1       2       lj/expand        1      " << sigma_hc << "     " << (vlp_diameter + ligand_diameter)/2 - sigma_hc << "	" << (vlp_diameter + ligand_diameter)/2 * 1.12246 << "  # epsilon, sigma_hc, delta_V-L, cut-off" << endl;
   
-  outfile << "pair_coeff      2       2       lj/cut           1      0.1      0.1122       # L-L" << endl;
+  outfile << "pair_coeff      2       2       lj/cut           1      " << ligand_diameter << "     " << ligand_diameter*1.12246 << "       # L-L" << endl;
   
-  outfile << "pair_coeff      1       3       lj/expand        1      0.12     0.79   1.02  # V-W" << endl;
+  outfile << "pair_coeff      1       3       lj/expand        1      " << sigma_hc << "     " << (vlp_diameter + wall_diameter)/2 - sigma_hc << "	" << (vlp_diameter + wall_diameter)/2 * 1.12246 << "  # V-W" << endl;
   
-  outfile << "pair_coeff      2       3       lj/expand        5      0.12     0.03   0.17  # L-W" << endl;
+  outfile << "pair_coeff      2       3       lj/cut       " << epsilon << "	   " << ligand_diameter << "	" << "2.5" << "  # L-W" << endl;
   
-  outfile << "pair_coeff      3       3       lj/cut           0      0.2                   # W-W" << endl << endl;
+  outfile << "pair_coeff      3       3       lj/cut           0      0               # W-W" << endl << endl;
   
   outfile << "#pair_modify  shift   yes     # the additive e_LJ for repulsion-only" << endl << endl;
   
@@ -288,17 +335,17 @@ int main() {
   
   outfile << "bond_style harmonic" << endl;
   
-  outfile << "bond_coeff 1    1000.0  0.809   # type, K, distance" << endl;
+  outfile << "bond_coeff 1    1000.0  " << type1_length << "  # type, K, distance" << endl;
   
-  outfile << "bond_coeff 2    1000.0  0.270" << endl;
+  outfile << "bond_coeff 2    1000.0  " << type2_length << endl;
   
-  outfile << "bond_coeff 3    1000.0  0.500" << endl << endl;
+  outfile << "bond_coeff 3    1000.0  " << type3_length << endl << endl;
   
   //Ensemble fixes
   
   outfile << "## Ensemble Fixes (+ for output) ##" << endl;
   
-  outfile << "variable        myTStep equal   0.001 # timestep where 1 MD step is 752.72 ps" << endl;
+  outfile << "variable        myTStep equal   0.001 # timestep where 1 MD step is " << sigma * sqrt( (mass_SI / 590) / (epsilon_SI) )  << " seconds" << endl;
   
   outfile << "timestep        ${myTStep}" << endl;
   
